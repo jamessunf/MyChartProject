805f1de94dd4b2355567d95b9a9c5fc05747747e
Feng SUN <42902314+jamessunf@users.noreply.github.com>
2019-10-04 11:09:24 -0400

    add receiverAdapter & senderAdapter

--- app/src/main/java/com/example/mylistview/MainActivity.java
+++ app/src/main/java/com/example/mylistview/MainActivity.java
@@ -31,23 +31,56 @@ public class MainActivity extends AppCompatActivity {
 
         ListView listview = (ListView) findViewById(R.id.list_chart);
 
-        MyAdapter adpt = new MyAdapter(this,msg,img);
-        listview.setAdapter(adpt);
+       // MyAdapter adpt = new MyAdapter(this,msg,img);
+       // listview.setAdapter(adpt);
 
 
     }
 
-    class MyAdapter extends ArrayAdapter<String>{
+    class ReceiverAdapter extends ArrayAdapter<String>{
 
         Context context;
         String rMsg[];
         int rImg[];
 
 
-         MyAdapter(Context c, String msg[],int img[]) {
-             super(c,R.layout.row_send,R.id.txt_send,msg);
+         ReceiverAdapter(Context c, String msg[],int img[]) {
+             super(c,R.layout.row_receiver,R.id.txt_send,msg);
 
              this.context = c;
+            this.rMsg = msg;
+            this.rImg = img;
+        }
+
+        @NonNull
+        @Override
+        public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+            LayoutInflater layoutInflater = (LayoutInflater)getApplicationContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+            View row = layoutInflater.inflate(R.layout.row_receiver,parent, false);
+            ImageView images = row.findViewById(R.id.img_send);
+            TextView message = row.findViewById(R.id.txt_send);
+
+            images.setImageResource(rImg[position]);
+            message.setText(rMsg[position]);
+
+
+            return row;
+        }
+    }
+
+
+    class SendAdapter extends ArrayAdapter<String>{
+
+        Context context;
+        String rMsg[];
+        int rImg[];
+
+
+        SendAdapter(Context c, String msg[],int img[]) {
+            super(c,R.layout.row_send,R.id.txt_send,msg);
+
+            this.context = c;
             this.rMsg = msg;
             this.rImg = img;
         }
